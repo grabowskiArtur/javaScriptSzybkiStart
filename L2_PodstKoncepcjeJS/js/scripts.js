5477e69596bd199838e6b0e1782b220f66b45aa2
Arturo <grabowski.a93@gmail.com>
2019-04-17 15:31:59 +0200

    All works

---
+++ L2_PodstKoncepcjeJS/js/scripts.js
@@ -0,0 +1,51 @@
+let name = "Artur"; // 'Artur' tez bedzie dzialac !
+var name2 ; //deklaracja bez definicji tez jest git
+var nameAndSurname = name + " " + "Grabowski";
+var cena = 5000; //typ number
+
+//automatycznie sie robi bool
+var isTrue = true;
+
+var empty = null; // jego typeof zwraca "object" !!
+var niezdefiniowana = undefined; // jak nie dasz "= undefined" to automatycznie bedzie undefined.
+
+var whatType = typeof "Piotr";
+
+cena += 2;
+
+var liczba =8;
+liczba %=5; // za liczbe bedzie 3 czyli reszta z dzielenia
+
+if ("")
+{
+    console.log("TRUE");
+} else {
+    console.log("FALSE")
+
+}
+console.log("typ tablicy to : " + typeof(tablica));
+var tablica = ["Piotr",1,2];
+
+for (var i = 0; i < 10 ; ++i )
+{
+    console.log(tablica[i]);
+}
+
+var Human = {
+    name: "Piotr",
+    age: 21
+};
+console.log(Human.age); // <--- tu odwolanie, jak w Javie
+
+function saySth(name, age) {
+    console.log("My name is " + name);
+    console.log("I have " + age + " years old");
+}
+saySth(77,Human.age);
+
+function dodaj(liczba1, liczba2 ) {
+    return liczba1 + liczba2;
+}
+
+console.log(dodaj());
+console.log(dodaj(2,1));
